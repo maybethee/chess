c77b9b975d6e95f25306eadbf523ebb236044937
maybethee <jozalamun@gmail.com>
2023-10-01 10:03:13 -0400

    Group related legality methods into modules

---
+++ path_clear.rb
@@ -0,0 +1,77 @@
+module PathClear
+  # path clear methods
+
+  def vertical_path_clear?
+    current_origin = @move.origin_coordinates.dup
+    current_destination = @move.destination_coordinates.dup
+
+    if @move.destination_coordinates[0] > @move.origin_coordinates[0]
+      until current_origin[0] >= current_destination[0] - 1
+        current_origin[0] += 1
+        puts "(adding) current origin after increment: #{current_origin[0]}"
+        return false unless @move.current_game_board[current_origin[0]][current_origin[1]].empty?
+      end
+    else
+      until current_origin[0] <= current_destination[0] + 1
+        current_origin[0] -= 1
+        puts "(subtract) current origin after increment: #{current_origin[0]}"
+        return false unless @move.current_game_board[current_origin[0]][current_origin[1]].empty?
+      end
+    end
+    true
+  end
+
+  def horizontal_path_clear?
+    puts "doing horizontal\n\n"
+    current_origin = @move.origin_coordinates.dup
+    current_destination = @move.destination_coordinates.dup
+
+    if @move.destination_coordinates[1] > @move.origin_coordinates[1]
+      until current_origin[1] >= current_destination[1] - 1
+        current_origin[1] += 1
+        puts "(adding) current origin after increment: #{current_origin[0]}"
+        return false unless @move.current_game_board[current_origin[0]][current_origin[1]].empty?
+      end
+    else
+      until current_origin[1] <= current_destination[1] + 1
+        current_origin[1] -= 1
+        puts "(subtract) current origin after increment: #{current_origin[0]}"
+        return false unless @move.current_game_board[current_origin[0]][current_origin[1]].empty?
+      end
+    end
+    true
+  end
+
+  # need to think of how to make this more concise...
+  def diagonal_path_clear?
+    current_origin = @move.origin_coordinates.dup
+    current_destination = @move.destination_coordinates.dup
+
+    if @move.destination_coordinates[1] > @move.origin_coordinates[1] && @move.destination_coordinates[0] > @move.origin_coordinates[0]
+      until current_origin[1] >= current_destination[1] - 1
+        current_origin[0] += 1
+        current_origin[1] += 1
+        return false unless @move.current_game_board[current_origin[0]][current_origin[1]].empty?
+      end
+    elsif @move.destination_coordinates[1] > @move.origin_coordinates[1] && @move.destination_coordinates[0] < @move.origin_coordinates[0]
+      until current_origin[1] >= current_destination[1] - 1
+        current_origin[0] -= 1
+        current_origin[1] += 1
+        return false unless @move.current_game_board[current_origin[0]][current_origin[1]].empty?
+      end
+    elsif @move.destination_coordinates[1] < @move.origin_coordinates[1] && @move.destination_coordinates[0] < @move.origin_coordinates[0]
+      until current_origin[1] <= current_destination[1] + 1
+        current_origin[0] -= 1
+        current_origin[1] -= 1
+        return false unless @move.current_game_board[current_origin[0]][current_origin[1]].empty?
+      end
+    elsif @move.destination_coordinates[1] < @move.origin_coordinates[1] && @move.destination_coordinates[0] > @move.origin_coordinates[0]
+      until current_origin[1] <= current_destination[1] + 1
+        current_origin[0] += 1
+        current_origin[1] -= 1
+        return false unless @move.current_game_board[current_origin[0]][current_origin[1]].empty?
+      end
+    end
+    true
+  end
+end
